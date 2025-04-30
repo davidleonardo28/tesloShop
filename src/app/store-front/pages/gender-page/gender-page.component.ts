e62bc2f50cd7b9556abab62bfb70c617e8096b57
David Barrera <store.things975@gmail.com>
2025-04-29 22:47:42 -0500

    DB New changes

--- src/app/store-front/pages/gender-page/gender-page.component.ts
+++ src/app/store-front/pages/gender-page/gender-page.component.ts
@@ -1,9 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { ProductCardComponent } from '@products/components/product-card/product-card.component';
+import { ProductsService } from '@products/services/product.service';
+import { map } from 'rxjs';
+import { PaginationComponent } from '../../../shared/components/pagination/pagination.component';
+import { PaginationService } from '@shared/components/pagination/pagination.service';
 
 @Component({
   selector: 'app-gender-page',
   standalone: true,
-  imports: [],
+  imports: [ProductCardComponent, PaginationComponent],
   templateUrl: './gender-page.component.html',
 })
-export class GenderPageComponent { }
+export class GenderPageComponent {
+  route = inject(ActivatedRoute);
+  productsService = inject(ProductsService);
+  paginationService = inject(PaginationService);
+
+  gender = toSignal(this.route.params.pipe(map(({ gender }) => gender)));
+
+  productsResource = rxResource({
+    request: () => ({
+      gender: this.gender(),
+      page: this.paginationService.currentPage() - 1,
+    }),
+    loader: ({ request }) => {
+      return this.productsService.getProducts({
+        gender: request.gender,
+        offset: request.page * 9,
+      });
+    },
+  });
+}
--- src/app/store-front/pages/product-page/product-page.component.ts
+++ src/app/store-front/pages/product-page/product-page.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { ProductsService } from '@products/services/product.service';
+import { ProductCarrouselComponent } from '../../../products/components/product-carrousel/product-carrousel.component';
 
 @Component({
   selector: 'app-product-page',
   standalone: true,
-  imports: [],
+  imports: [ProductCarrouselComponent],
   templateUrl: './product-page.component.html',
 })
-export class ProductPageComponent { }
+export class ProductPageComponent {
+  activatedRoute = inject(ActivatedRoute);
+  productService = inject(ProductsService);
+
+  productIdslug = this.activatedRoute.snapshot.params['idSlug'];
+
+  productResource = rxResource({
+    request: () => ({ idSlug: this.productIdslug }),
+    loader: ({ request }) =>
+      this.productService.getProductByIdSlug(request.idSlug),
+  });
+}
